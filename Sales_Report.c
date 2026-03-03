1b9034982a71dab802514508d3942e10f7c2e5fe
Jerry Merveille <jerrymerv10@gmail.com>
2026-03-03 14:36:33 -0600

    Initial commit. Sales_Report.c was added to stage  and contains basic program structures, including header comments, stdio.h library, and a  main() function.

---
+++ Sales_Report.c
@@ -0,0 +1,15 @@
+// EECS 348 Lab 05 - Sales Report
+// Name: Jerry Merveille
+// KUID: 3203634
+// LAB session: Thursday 9 AM - 10:50 AM
+// LAB Assignment: 01
+// Description: This program reads 12 monthly sales and generate a sales report.
+// The program display the month and sales; show the minimum, maximum, and average monthly sales; 
+// generate a six-month moving average report; and display the sales from highest to lowest.
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
