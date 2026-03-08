90e2c7076d575e1323c3f5e893e7ac9f923543e3
Jerry Merveille <jerrymerv10@gmail.com>
2026-03-08 16:13:41 -0500

    The functions calculate_average(), moving_average_report(), sort_descending(), and descending_sales_report() have been implemented.

--- Sales_Report.c
+++ Sales_Report.c
@@ -40,26 +40,76 @@ int main() {
 void print_sales_report(float sales[], char *months[12]) {
     printf("%-10s %15s\n", "Month", "Sales");
     for (int i = 0; i < 12; i++) {
-        printf("%-10s %15.2f\n", months[i], sales[i]);
+        printf("%-10s $%14.2f\n", months[i], sales[i]);
     }
 }
 
 void sales_summary_report(float sales[], char *months[12]) {
+    
     float min = sales[0];
     float max = sales[0];
+
+    int month_min = 0;
+    int month_max = 0;
 
     for (int i = 0; i < 12; i++) {
         if (sales[i] < min) {
             min = sales[i];
+            month_min = i;
         }
         if (sales[i] > max) {
             max = sales[i];
+            month_max = i;
         }
     }
 
     float average_sales = calculate_average(sales, 0, 11);
 
-    printf("Minimum Sales: %.2f\n", min);
-    printf("Maximum Sales: %.2f\n", max);
+    printf("Minimum Sales: %.2f (%s)\n", min, months[month_min]);
+    printf("Maximum Sales: %.2f (%s)\n", max, months[month_max]);
     printf("Average Sales: %.2f\n", average_sales);
+}
+
+float calculate_average(float sales[], int start_index, int end_index) {
+    float sum = 0.0;
+    for (int i = start_index; i <= end_index; i++) {
+        sum += sales[i];
+    }
+    return (float)(sum / (end_index - start_index + 1));
+}
+
+void moving_average_report(float sales[], char *months[12]) {
+    for (int i = 0; i <= 6; i++) {
+        float average = calculate_average(sales, i, i + 5);
+        printf("%s - %s: %.2f\n", months[i], months[i + 5], average);
+    }
+}
+
+void sort_descending(float sales[], char *months[12]) {
+
+    for (int i = 0; i < 12; i++) {
+        float max_sales = sales[i];
+        int max_index = i;
+
+        for (int j = i; j < 12; j++) {
+            if (sales[j] > max_sales) {
+                max_sales = sales[j];
+                max_index = j;
+            }
+        }
+        // Swap sales
+        float temp_sales = sales[max_index];
+        sales[max_index] = sales[i];
+        sales[i] = temp_sales;
+
+        // Swap months
+        char *temp_month = months[max_index];
+        months[max_index] = months[i];
+        months[i] = temp_month;
+    }
+}
+
+void descending_sales_report(float sales[], char *months[12]) {
+    sort_descending(sales, months);
+    print_sales_report(sales, months);
 }
